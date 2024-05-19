6596225d0d09b6ecc361287bf19f35f3b94c77d7
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-05-19 09:10:00 +0530

    Update stats.py

--- nexichat/modules/stats.py
+++ nexichat/modules/stats.py
@@ -2,8 +2,8 @@ from pyrogram import filters, Client
 from pyrogram.types import Message
 
 from nexichat import OWNER, nexichat
-from nexichat.database.chats import get_served_chats
-from nexichat.database.users import get_served_users
+from nexichat.database.chatsdb import get_served_chats
+from nexichat.database.usersdb import get_served_users
 
 
 @nexichat.on_message(filters.command("stats") & filters.user(OWNER))
