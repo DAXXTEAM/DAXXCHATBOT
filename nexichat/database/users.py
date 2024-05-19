370bee2c2fbf1c545a5e6c5679f57fb54435a12e
‹ 𝚲 𝚼 𝐈 › <164884298+Notookk@users.noreply.github.com>
2024-05-19 09:42:33 +0530

    Update users.py

--- nexichat/database/users.py
+++ nexichat/database/users.py
@@ -1,34 +1,24 @@
 from nexichat import db
 
-chatsdb = db.chatsdb
+usersdb = db.users
 
 
-async def get_served_chats() -> list:
-    chats = chatsdb.find({"chat_id": {"$lt": 0}})
-    if not chats:
-        return []
-    chats_list = []
-    for chat in await chats.to_list(length=1000000000):
-        chats_list.append(chat)
-    return chats_list
-
-
-async def is_served_chat(chat_id: int) -> bool:
-    chat = await chatsdb.find_one({"chat_id": chat_id})
-    if not chat:
+async def is_served_user(user_id: int) -> bool:
+    user = await usersdb.find_one({"user_id": user_id})
+    if not user:
         return False
     return True
 
 
-async def add_served_chat(chat_id: int):
-    is_served = await is_served_chat(chat_id)
+async def get_served_users() -> list:
+    users_list = []
+    async for user in usersdb.find({"user_id": {"$gt": 0}}):
+        users_list.append(user)
+    return users_list
+
+
+async def add_served_user(user_id: int):
+    is_served = await is_served_user(user_id)
     if is_served:
         return
-    return await chatsdb.insert_one({"chat_id": chat_id})
-
-
-async def remove_served_chat(chat_id: int):
-    is_served = await is_served_chat(chat_id)
-    if not is_served:
-        return
-    return await chatsdb.delete_one({"chat_id": chat_id})
+    return await usersdb.insert_one({"user_id": user_id})
